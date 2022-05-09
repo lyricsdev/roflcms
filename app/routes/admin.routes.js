0d76387dea51445bcf1c1313837fd9f329a04e31
Алексей <77627599+lyricsdev@users.noreply.github.com>
2022-05-09 12:05:42 +0300

    функционал для админки

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/user.controller");
 
 const ServerController = require("../controllers/server.controller");
 const newscontroller = require("../controllers/news.controller");
-
+const adminservice = require("../services/admin.service");
 module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header(
@@ -12,7 +12,14 @@ module.exports = function(app) {
     );
     next();
   });
-  app.post("/api/rconsend",[authJwt.verifyToken, authJwt.isAdmin],ServerController.rconsend)
-  app.post("/api/addnews",[authJwt.verifyToken, authJwt.isAdmin],newscontroller.addnews)
-
+  app.post("/api/admin/rconsend",[authJwt.verifyToken, authJwt.isAdmin],ServerController.rconsend)
+  app.post("/api/admin/addnews",[authJwt.verifyToken, authJwt.isAdmin],newscontroller.addnews)
+  app.post("/api/admin/givemoneytouser",[authJwt.verifyToken, authJwt.isAdmin],adminservice.givemoneytouser)
+  app.post("/api/admin/addserver",[authJwt.verifyToken, authJwt.isAdmin],adminservice.addserver)
+  app.post("/api/admin/getallusers",[authJwt.verifyToken, authJwt.isAdmin],adminservice.getallusers)
+  app.post('/api/admin/edituser',[authJwt.verifyToken, authJwt.isAdmin],adminservice.edituser)
+  app.post('/api/admin/editserver',[authJwt.verifyToken, authJwt.isAdmin],adminservice.editserver)
+  app.post('/api/admin/getservers',[authJwt.verifyToken, authJwt.isAdmin],adminservice.getservers)
+  app.post('/api/admin/getnews',[authJwt.verifyToken, authJwt.isAdmin],adminservice.getnews)
+  app.post('/api/admin/editnews',[authJwt.verifyToken, authJwt.isAdmin],adminservice.editnews)
 };
